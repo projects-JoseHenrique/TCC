07faa4e670c5277466d01d9335bcdfe3d62e186f
Zeca <projetosjosehenrique@gmail.com>
2023-09-03 00:18:52 -0300

    Arrumando página home.php do PDV

--- login/painel-operador/index.php
+++ login/painel-operador/index.php
@@ -42,7 +42,7 @@ $id_usu = $res[0]['id'];
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-    	<img src="../img/logo-texto-escuro.png" width="100">
+    	<img src="../img/logo-painel.png" width="100">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
